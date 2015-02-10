108b32f8b6101acd972520753efea3351ad33826
jsanch <jaime.sanchez.arias@gmail.com>
2015-02-09 22:35:29 -0500

    added javascript to seek desired second

---
+++ javascripts/youtubeapi.js
@@ -0,0 +1,29 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '400',
+    width: '600',
+    videoId: element.getAttribute("youtube-vid-id"),
+
+  });
+}
+function seekTo(seconds) {
+	player.seekTo(seconds);
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
+
+element = document.getElementById("player");
+console.log(element.getAttribute("youtube-vid-id"));
+
