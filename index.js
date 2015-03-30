8c641cba0fbd807c98b15f18a4eaf655a1deb0f2
jsanch <jaime.sanchez.arias@gmail.com>
2015-03-30 02:28:17 -0400

    fixed

---
+++ index.js
@@ -0,0 +1,18 @@
+$(function() {
+    var BV = new $.BigVideo({container:$('#intro-video')});
+	BV.init();
+
+
+	if (Modernizr.touch) {
+	    BV.show('assets/fallback.jpg');
+	} else {
+		BV.show('assets/video/Intro_loop.mp4', {ambient:true});
+	}
+
+
+  	// var overlay= document.getElementById('arrow-overlay');
+  	// $('#arrow-overlay').css.color = '#000';
+
+
+});
+
